c89249ab207d01ced17e96684afd7b343e7c83e5
Andrew <adiana1175@gmail.com>
2018-01-01 16:08:20 -0500

    still testing

--- index.js
+++ index.js
@@ -113,45 +113,24 @@ function handleMessage(sender_psid, received_message) {
               template_type: "generic",
               elements: {
                 [
-                  "title": "Welcome to Peter'\''s Hats",
-                  "image_url": "https://petersfancybrownhats.com/company_image.png",
-                  "subtitle": "We'\''ve got the right hat for everyone.",
-                  "default_action": {
-                    "type": "web_url",
-                    "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
-                    "messenger_extensions": true,
-                    "webview_height_ratio": "tall",
-                    "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                  title: "Welcome to Peter'\''s Hats",
+                  image_url: "https://petersfancybrownhats.com/company_image.png",
+                  subtitle: "We'\''ve got the right hat for everyone.",
+                  default_action: {
+                    type: "web_url",
+                    url: "https://peterssendreceiveapp.ngrok.io/view?item=103",
+                    messenger_extensions: true,
+                    webview_height_ratio: "tall",
+                    fallback_url: "https://peterssendreceiveapp.ngrok.io/"
                   },
-                  "buttons": [{
-                    "type": "web_url",
-                    "url": "https://petersfancybrownhats.com",
-                    "title": "View Website"
+                  buttons: [{
+                    type: "web_url",
+                    url: "https://petersfancybrownhats.com",
+                    title: "View Website"
                   }, {
-                    "type": "postback",
-                    "title": "Start Chatting",
-                    "payload": "DEVELOPER_DEFINED_PAYLOAD"
-                  }]
-                ],
-                [
-                  "title": "Welcome to Peter'\''s Hats",
-                  "image_url": "https://petersfancybrownhats.com/company_image.png",
-                  "subtitle": "We'\''ve got the right hat for everyone.",
-                  "default_action": {
-                    "type": "web_url",
-                    "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
-                    "messenger_extensions": true,
-                    "webview_height_ratio": "tall",
-                    "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                  },
-                  "buttons": [{
-                    "type": "web_url",
-                    "url": "https://petersfancybrownhats.com",
-                    "title": "View Website"
-                  }, {
-                    "type": "postback",
-                    "title": "Start Chatting",
-                    "payload": "DEVELOPER_DEFINED_PAYLOAD"
+                    type: "postback",
+                    title: "Start Chatting",
+                    payload: "DEVELOPER_DEFINED_PAYLOAD"
                   }]
                 ]
 
